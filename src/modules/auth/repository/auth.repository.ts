5a9869a9bb6e8b45ec444e5e4377f5d34b7fb328
dungpt99 <69756171+dungpt99@users.noreply.github.com>
2022-10-10 13:19:33 +0700

    Delete src/modules/auth directory

--- src/modules/auth/repository/auth.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { TokenEntity } from '../entities/auth.entity';
-
-@EntityRepository(TokenEntity)
-export class AuthRepository extends Repository<TokenEntity> {}
