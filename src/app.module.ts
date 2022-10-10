8ca5664555d23e3205a6e1354b5d0778229d14c8
dungpt <dungpt1@vmodev.com>
2022-10-10 13:51:02 +0700

    fix(backend): error import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,29 @@
+import { JwtAuthGuard } from "./modules/Auth/jwt-auth.guard";
 import { Module } from "@nestjs/common";
 import { typeOrmConfig } from "./config/database/typeorm.config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserModule } from "./modules/user/user.module";
-import { AuthModule } from "./modules/auth/auth.module";
 import { PostModule } from "./modules/post/post.module";
 import { APP_GUARD } from "@nestjs/core";
-import { JwtAuthGuard } from "./modules/auth/jwt-auth.guard";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
-
+import { AuthModule } from "./modules/Auth/auth.module";
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule,
-    UserModule,
-    PostModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "public"),
-    }),
-  ],
-  controllers: [],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-  ],
+	imports: [
+		TypeOrmModule.forRoot(typeOrmConfig),
+		AuthModule,
+		UserModule,
+		PostModule,
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, "..", "public"),
+		}),
+	],
+	controllers: [],
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: JwtAuthGuard,
+		},
+	],
 })
 export class AppModule {}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,33 +1,33 @@
-import { TokenEntity } from "src/modules/auth/entities/auth.entity";
+import { TokenEntity } from "src/modules/Auth/entities/auth.entity";
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  OneToMany,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
+	Column,
+	CreateDateColumn,
+	Entity,
+	OneToMany,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
 } from "typeorm";
 
 @Entity({ name: "users" })
 export class UserEntity {
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
-  @Column({ name: "username", type: "varchar", length: 30, unique: true })
-  username: string;
+	@Column({ name: "username", type: "varchar", length: 30, unique: true })
+	username: string;
 
-  @Column({ name: "password", type: "varchar", length: 200 })
-  password: string;
+	@Column({ name: "password", type: "varchar", length: 200 })
+	password: string;
 
-  @OneToMany(() => TokenEntity, (token) => token.user)
-  tokens: TokenEntity[];
+	@OneToMany(() => TokenEntity, (token) => token.user)
+	tokens: TokenEntity[];
 
-  @CreateDateColumn({ type: "timestamp with time zone" })
-  createdAt: Date;
+	@CreateDateColumn({ type: "timestamp with time zone" })
+	createdAt: Date;
 
-  @UpdateDateColumn({ type: "timestamp with time zone" })
-  updatedAt: Date;
+	@UpdateDateColumn({ type: "timestamp with time zone" })
+	updatedAt: Date;
 
-  @Column({ name: "status", type: "boolean", default: true })
-  status: boolean;
+	@Column({ name: "status", type: "boolean", default: true })
+	status: boolean;
 }
