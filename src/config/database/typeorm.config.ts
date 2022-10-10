ea3eea9d6c52b538d1d46f4dffd32f703c4f5a2d
dungpt <dungpt1@vmodev.com>
2022-10-10 13:55:13 +0700

    fix(backend): ssl

--- src/config/database/typeorm.config.ts
+++ src/config/database/typeorm.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { databaseConfig } from "../configs.constants";
 
 export const typeOrmConfig: TypeOrmModule = {
-  host: databaseConfig.host,
-  type: databaseConfig.type,
-  username: databaseConfig.username,
-  password: databaseConfig.password,
-  database: databaseConfig.database,
-  entities: [`${__dirname}/../../**/*.entity.{js,ts}`],
-  synchronize: true,
-  // ssl: { rejectUnauthorized: false }
+	host: databaseConfig.host,
+	type: databaseConfig.type,
+	username: databaseConfig.username,
+	password: databaseConfig.password,
+	database: databaseConfig.database,
+	entities: [`${__dirname}/../../**/*.entity.{js,ts}`],
+	synchronize: true,
+	ssl: { rejectUnauthorized: false },
 };
